4acfac6df1477762174ba3d328bcd787d05373c9
hb <82237720+hbbots@users.noreply.github.com>
2022-06-30 22:35:00 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -45,6 +45,15 @@ async def progress_for_pyrogram(
         except:
             pass
 
+def format_bytes(size):
+    # 2**10 = 1024
+    power = 2**10
+    n = 0
+    power_labels = {0 : '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return size, power_labels[n]+'B'
 
 def humanbytes(size):
     # https://stackoverflow.com/a/49361727/4723940
